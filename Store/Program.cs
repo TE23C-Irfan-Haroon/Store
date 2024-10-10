ab0988e63e60da145e5ce13307217c3e4b6996cb
Haroon Irfan <haroon.irfan@elev.ga.ntig.se>
2024-10-10 13:26:32 +0200

    first commit

---
+++ Store/Program.cs
@@ -0,0 +1,41 @@
+﻿
+int i = 0;
+
+while (i < 10)
+{
+    Console.WriteLine("hej");
+    i++;
+}
+
+for (int j = 0; j < 10; j++)
+{
+
+}
+
+// int ageNum = 0;
+// bool succes = false;
+
+// while (succes == false )
+// {
+// Console.WriteLine("Hur gammal är du?");
+// string age = Console.ReadLine();
+// succes = int.TryParse(age, out ageNum);
+
+// if (succes == false)
+// {
+//     Console.WriteLine("Du måste skriva en siffra");
+// }  
+// }
+
+
+
+// if (ageNum < 18)
+// {
+//     Console.WriteLine("Du är för ung");
+// }
+// else
+// {
+//     Console.WriteLine("Välkommen");
+// }
+
+// Console.ReadLine();
