3363c615ff1c8cb33f0ff746f716497f68f216f6
Haroon Irfan <haroon.irfan@elev.ga.ntig.se>
2024-10-18 13:41:54 +0200

    V.1

--- Store/Program.cs
+++ Store/Program.cs
@@ -1,41 +1,106 @@
-﻿
-int i = 0;
+﻿Console.BackgroundColor = ConsoleColor.DarkGray;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Clear();
 
-while (i < 10)
+bool succes = false;
+int ageNum = 0;
+
+while (succes == false)
 {
-    Console.WriteLine("hej");
-    i++;
+    Console.WriteLine("How old are you?");
+    String age = Console.ReadLine();
+    succes = int.TryParse(age, out ageNum);
+
+    if (succes == false)
+    {
+        Console.WriteLine("WRITE A NUMBER DUMBASS!!!");
+    }
+    else if (ageNum < 18)
+    {
+        Console.WriteLine("You are too young!!!");
+        Console.ReadLine();
+        return;
+    }
+    else if (ageNum >= 18)
+    {
+        Console.WriteLine("Welcome to the SHOP");
+        Console.ReadLine();
+    }
 }
 
-for (int j = 0; j < 10; j++)
+
+int coins = 100;
+String shopChoice = ("");
+int shopChoicenum = 1;
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Clear();
+
+while (coins > 0)
 {
 
+    Console.WriteLine("Welcome to the Shop!");
+    Console.WriteLine($"You have {coins} coins.");
+    Console.WriteLine("1. Mostafa's Leg Piece - Extra Spicy - Only 19.99$");
+    Console.WriteLine("2. Mostafa's Crispy Breast - Extra Parmesan - Only 29.99$");
+    Console.WriteLine("3. Mostafa's Special - MYSTERY FLAVOUR - WARNING SHAPED LIKE BALLS - Only 69.99$");
+    Console.WriteLine("4. Exit Shop");
+    Console.WriteLine("Choose an option: 1, 2, 3, or 4");
+    shopChoice = Console.ReadLine();
+
+    int.TryParse(shopChoice, out shopChoicenum);
+    if (shopChoicenum < 1 || shopChoicenum > 4)
+    {
+        Console.WriteLine("Please choose a valid option.");
+    }
+
+    if (shopChoicenum == 1)
+    {
+        if (coins >= 20)
+        {
+            coins -= 20;
+
+        }
+        else
+        {
+            Console.WriteLine("Not enough coins.");
+        }
+    }
+    else if (shopChoicenum == 2)
+    {
+        if (coins >= 30)
+        {
+            coins -= 30;
+            Console.WriteLine($"You have {coins}");
+        }
+        else
+        {
+            Console.WriteLine("Not enough coins.");
+        }
+    }
+    else if (shopChoicenum == 3)
+    {
+        if (coins >= 70)
+        {
+            coins -= 70;
+            Console.WriteLine($"You have {coins}");
+        }
+        else
+        {
+            Console.WriteLine("Not enough coins.");
+        }
+    }
+    else if (shopChoicenum == 4)
+    {
+
+        break;
+    }
+    else if (coins <= 0)
+    {
+        Console.WriteLine("You have no coins left");
+        Console.ReadLine();
+    }
+    shopChoice = Console.ReadLine();
+    Console.Clear();
 }
 
-// int ageNum = 0;
-// bool succes = false;
-
-// while (succes == false )
-// {
-// Console.WriteLine("Hur gammal är du?");
-// string age = Console.ReadLine();
-// succes = int.TryParse(age, out ageNum);
-
-// if (succes == false)
-// {
-//     Console.WriteLine("Du måste skriva en siffra");
-// }  
-// }
-
-
-
-// if (ageNum < 18)
-// {
-//     Console.WriteLine("Du är för ung");
-// }
-// else
-// {
-//     Console.WriteLine("Välkommen");
-// }
-
-// Console.ReadLine();
--- Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0988e63e60da145e5ce13307217c3e4b6996cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
